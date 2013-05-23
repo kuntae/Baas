2003a10cfd67a039121a66e4bf3d5ac1632d8f93
kuntae <kuntae89@naver.com>
2013-05-23 23:31:30 +0900

    datatree&&css

--- bacas/routes/web/datatree.js
+++ bacas/routes/web/datatree.js
@@ -2,26 +2,93 @@ var db = require('./../db_structure');
 
 exports.datatree_user=function(req,res){
     console.log("data tree of user");
-    res.render('datatree_page', {
-        title: 'datatree page'
+    var idArray =[];
+    var mailArray=[];
+    var deviceArray=[];
+    var countValue;
+    db.userinfo.find({},function(err,doc){
+        countValue = doc.length;
+        for(var i=0;i<doc.length;i++){
+            idArray.push('\''+doc[i].id+'\'');
+            mailArray.push('\''+doc[i].mail+'\'');
+            deviceArray.push('\''+doc[i].deviceid+'\'');
+        }
+
+        res.render('datatree_page_user', {
+            title: 'datatree page',
+            idArray: idArray,
+            mailArray:mailArray,
+            deviceArray:deviceArray,
+            countValue: countValue
+        });
     });
 }
 exports.datatree_rank=function(req,res){
     console.log("data tree of rank");
-    res.render('datatree_page', {
-        title: 'datatree page'
+    var idArray =[];
+    var mailArray=[];
+    var deviceArray=[];
+    var countValue;
+    db.userinfo.find({},function(err,doc){
+        countValue = doc.length;
+        for(var i=0;i<doc.length;i++){
+            idArray.push('\''+doc[i].id+'\'');
+            mailArray.push('\''+doc[i].mail+'\'');
+            deviceArray.push('\''+doc[i].deviceid+'\'');
+        }
+
+        res.render('datatree_page_rank', {
+            title: 'datatree page',
+            idArray: idArray,
+            mailArray:mailArray,
+            deviceArray:deviceArray,
+            countValue: countValue
+        });
     });
 }
 exports.datatree_location=function(req,res){
-    console.log("data tree of location");
-    res.render('datatree_page', {
-        title: 'datatree page'
+    var idArray =[];
+    var mailArray=[];
+    var deviceArray=[];
+    var countValue;
+    db.userinfo.find({},function(err,doc){
+        countValue = doc.length;
+        for(var i=0;i<doc.length;i++){
+            idArray.push('\''+doc[i].id+'\'');
+            mailArray.push('\''+doc[i].mail+'\'');
+            deviceArray.push('\''+doc[i].deviceid+'\'');
+        }
+
+        res.render('datatree_page_location', {
+            title: 'datatree page',
+            idArray: idArray,
+            mailArray:mailArray,
+            deviceArray:deviceArray,
+            countValue: countValue
+        });
     });
 }
 exports.datatree_push=function(req,res){
     console.log("data tree of push");
-    res.render('datatree_page', {
-        title: 'datatree page'
+    var idArray =[];
+    var mailArray=[];
+    var deviceArray=[];
+    var countValue;
+    db.userinfo.find({},function(err,doc){
+        countValue = doc.length;
+        for(var i=0;i<doc.length;i++){
+            idArray.push('\''+doc[i].id+'\'');
+            mailArray.push('\''+doc[i].mail+'\'');
+            deviceArray.push('\''+doc[i].deviceid+'\'');
+        }
+
+        res.render('datatree_page_push', {
+            title: 'datatree page',
+            idArray: idArray,
+            mailArray:mailArray,
+            deviceArray:deviceArray,
+            countValue: countValue
+        });
     });
 }
 exports.datatree_page= function (req, res) {
