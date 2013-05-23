ebcdfd579a14250e6ac0c840378d0ce8f64952e7
kuntae <kuntae89@naver.com>
2013-05-23 21:43:31 +0900

    datatree

--- bacas/routes/web/datatree.js
+++ bacas/routes/web/datatree.js
@@ -1,3 +1,29 @@
+var db = require('./../db_structure');
+
+exports.datatree_user=function(req,res){
+    console.log("data tree of user");
+    res.render('datatree_page', {
+        title: 'datatree page'
+    });
+}
+exports.datatree_rank=function(req,res){
+    console.log("data tree of rank");
+    res.render('datatree_page', {
+        title: 'datatree page'
+    });
+}
+exports.datatree_location=function(req,res){
+    console.log("data tree of location");
+    res.render('datatree_page', {
+        title: 'datatree page'
+    });
+}
+exports.datatree_push=function(req,res){
+    console.log("data tree of push");
+    res.render('datatree_page', {
+        title: 'datatree page'
+    });
+}
 exports.datatree_page= function (req, res) {
     console.log("data tree");
     res.render('datatree_page', {
